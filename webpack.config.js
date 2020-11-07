982cb6da4e9e0e067588955d14c88d9f00202772
Mihail Marian <mihailmarian12@gmail.com>
2020-11-07 09:35:59 +0000

    prepared for image load

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,11 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
         use: [
           'file-loader',
         ],
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
