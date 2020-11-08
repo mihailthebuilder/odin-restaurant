90cc2647c758964349374457f101fc3ea18cb7ab
Mihail Marian <mihailmarian12@gmail.com>
2020-11-08 17:25:07 +0000

    added tab nav

--- src/js/page-load.js
+++ src/js/page-load.js
@@ -2,6 +2,7 @@ import html from '../html/page-load.html';
 
 const pageLoad = () => {
   document.getElementById('content').insertAdjacentHTML('beforeend',html);
+  document.querySelector('li:first-child').classList.add('selected');
 }
 
 export {pageLoad}
