586bf2e8d350aecc9c08bd486f29b442d37c00c6
Mihail Marian <mihailmarian12@gmail.com>
2020-11-08 14:28:44 +0000

    built in html-loader that works

--- src/js/page-load.js
+++ src/js/page-load.js
@@ -1,23 +1,7 @@
+import html from '../html/page-load.html';
 
 const pageLoad = () => {
-  
-  document.getElementById('content').insertAdjacentHTML('beforeend',`
-    <header>
-      <nav><h1>Odin Restaurants</h1></nav>
-    </header>
-
-    <section id="hero">
-      <h1>The Best Place to Eat Nordic Food</h1><button type="button">Find out more...</button>
-    </section>
-  
-    <section id="intro">
-      <h2>Why we're amazing</h2>
-      <div id="features">
-        <div id="chefs">
-        </div>
-      </div>
-    </section>
-  `);
+  document.getElementById('content').insertAdjacentHTML('beforeend',html);
 }
 
 export {pageLoad}
